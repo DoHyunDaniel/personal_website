78f07d1c4e7c11ce4d8b8c1d620b4ba8901f1c0a
dohyundaniel <kdhdaniel0506@gmail.com>
2023-12-03 03:44:21 +0900

    배포를 위한 환경변수 수정

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ module.exports ={
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+    database: process.env.DB_NAME,
     charset:"utf8mb4",
 };
